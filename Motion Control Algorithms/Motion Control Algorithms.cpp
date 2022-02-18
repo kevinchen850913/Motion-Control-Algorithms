40cb025abcaf41dc6ebce362fdd64d589139b816
kevinchen <kevinchen@aurotek.com>
2022-02-18 17:42:04 +0800

    Add decode cmd

--- Motion Control Algorithms/Motion Control Algorithms.cpp
+++ Motion Control Algorithms/Motion Control Algorithms.cpp
@@ -2,23 +2,43 @@
 //
 
 #include <iostream>;
+#include <string>
 #include <thread>;
+#include <queue>
 #include "MySystemThread.h";
 #include "DP_COMMAND.h";
 
 using namespace std;
+
 thread MySystemThread(MySystemThread_job);
+string CMD;
+string delimiter = " ";
+vector<string> words{};
+size_t pos;
+DP_COMMAND_DATA COMMAND_DATA;
 
 int main()
 {
-    string CMD;
     cout << "Hello World!\n";
     MySystemThread.join();
     while (true)
     {
-        cin >> CMD;
+        getline(cin, CMD);
+        if (CMD != "")
+        {
+            words.clear();
+            while ((pos = CMD.find(delimiter)) != string::npos) {
+                words.push_back(CMD.substr(0, pos));
+                CMD.erase(0, pos + delimiter.length());
+            }
+            COMMAND_DATA.cmd = stoi(words[0]);
+            COMMAND_DATA.sub_cmd = stoi(words[1]);
+            COMMAND_DATA.handle_id = stoi(words[2]);
+            COMMAND_DATA.data1 = stoi(words[3]);
+        }        
     }
 }
+
 
 
 
